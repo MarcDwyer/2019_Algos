c8e8b1c769a3aa916fef851c62814175e6a34fe0
Marc Dwyer <marcdwyer1738@gmail.com>
2019-10-15 01:01:01 -0500

    added search func to btree

--- trees/binarytree.js
+++ trees/binarytree.js
@@ -2,13 +2,13 @@ function BinaryTree() {
     this.root = null
 }
 
-function Node(value){
+function Node(value) {
     this.value = value
     this.left = null
     this.right = null
 }
 
-BinaryTree.prototype.insert = function(val){
+BinaryTree.prototype.insert = function (val) {
     let newNode = new Node(val)
     if (!this.root) {
         this.root = newNode
@@ -17,16 +17,45 @@ BinaryTree.prototype.insert = function(val){
     } else {
         let curr = this.root
         while (true) {
+            if (curr.value === val) return null;
             if (val < curr.value) {
                 if (!curr.left) {
                     console.log('wtf')
                     curr.left = newNode
                     return this
+                }
+                curr = curr.left
+            } else if (val > curr.value) {
+                if (!curr.right) {
+                    curr.right = newNode
+                    return this
+                }
+                curr = curr.right
+            }
+        }
+    }
+}
+
+BinaryTree.prototype.search = function (value) {
+    let count = 0;
+    if (this.root) {
+        let curr = this.root
+        while (true) {
+            if (curr.value === value || count > 100) return curr;
+            if (value > curr.value) {
+                if (curr.right) {
+                    curr = curr.right
                 } else {
-                    console.log('this ran')
+                    return null
+                }
+            } else if (value < curr.value) {
+                if (curr.left) {
                     curr = curr.left
+                } else {
+                    return null;
                 }
             }
+            count++
         }
     }
 }
@@ -36,6 +65,9 @@ let tree = new BinaryTree()
 tree.insert(10)
 tree.insert(5)
 console.log(tree)
+tree.insert(12)
+tree.insert(7)
+tree.insert(17)
 tree.insert(2)
 
 console.log(tree)
